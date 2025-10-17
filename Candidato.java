7a9dd91b5d43dd4be225e2cde0f72fd599860899
Al3x-C4rr <jmcarrerita07@gmail.com>
2025-10-17 13:18:42 -0600

    Update Candidato.java

--- Candidato.java
+++ Candidato.java
@@ -4,7 +4,6 @@ public class Candidato {
     private String experiencia;
     private String estadoCivil;
     private int edad;
-    private Empresa empresa; 
 
     public Candidato(){
         this.genero = "";
@@ -14,21 +13,12 @@ public class Candidato {
         this.edad = 0;
     }
 
-    public void completarPerfil(String genero, String discapacidad, String experiencia, String estadoCivil, int edad) {
+    public Candidato(String genero, String discapacidad, String experiencia, String estadoCivil, int edad) {
         this.genero = genero;
         this.discapacidad = discapacidad;
         this.experiencia = experiencia;
         this.estadoCivil = estadoCivil;
         this.edad = edad;
-    }
-
-    public void buscarEmpresas(List<Empresa> empresas) {
-        System.out.println("Empresas que coinciden con tu perfil:");
-        for (Empresa empresa : empresas) {
-            if (empresa.aceptaCandidato(this)) {
-                System.out.println("- " + empresa.getNombre());
-            }
-        }
     }
 
     public String getGenero() {
